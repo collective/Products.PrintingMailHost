cd42e4668bdac253003a10f8bb961e3f67915f18
Maurits van Rees <m.van.rees@zestsoftware.nl>
2010-01-05 03:45:06 +0000

    version was not updated after last tag

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.5dev'
+version = '0.6dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
