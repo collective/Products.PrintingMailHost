b5ad971dcac58045b9a7382360f97050d6e6b766
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-12-20 18:27:49 +0100

    chore: adjust dependencies

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=[
         "setuptools",
-        "six",
+        "Products.MailHost",
+        "Zope",
     ],
 )
