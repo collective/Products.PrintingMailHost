f25aa3a329108cfed1ca0edd4c48e0c6afc17f59
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-12-20 18:30:13 +0100

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -16,19 +15,13 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 5.2",
-        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="zope debug mailhost",
@@ -36,14 +29,10 @@ setup(
     author_email="plone-developers@lists.sourceforge.net",
     url="https://github.com/collective/Products.PrintingMailHost",
     license="GPL",
-    packages=find_packages("src"),
-    namespace_packages=["Products"],
-    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    python_requires=">=3.10",
     install_requires=[
-        "setuptools",
         "Products.MailHost",
         "Zope",
     ],
