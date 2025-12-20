ce87b3c5cf9e2204ddc78a8c8139b9b2c66ce8cf
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-12-20 18:30:13 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 long_description = Path("README.md").read_text() + "\n" + Path("CHANGES.md").read_text()
 
-version = "1.1.9.dev0"
+version = "2.0.0.dev0"
 
 setup(
     name="Products.PrintingMailHost",
