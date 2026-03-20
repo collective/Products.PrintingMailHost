95aa269650ee31b09a10d23961a8ea5d86a62e07
Maurits van Rees <maurits@vanrees.org>
2026-03-20 18:47:15 +0100

    Remove unneeded comment.

--- src/Products/PrintingMailHost/testing.py
+++ src/Products/PrintingMailHost/testing.py
@@ -4,8 +4,6 @@ from plone.testing import zope
 
 
 class PrintingMailHostFixture(PloneSandboxLayer):
-    # defaultBases = (AUTOLOGIN_LIBRARY_FIXTURE,)
-
     def setUpZope(self, app, configurationContext):
         zope.installProduct(app, "Products.PrintingMailHost")
 
